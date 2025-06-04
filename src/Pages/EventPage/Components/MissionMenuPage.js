43a45928aff82c37339174d671082fad6dac80e2
livingdavid <livingdavid@naver.com>
2025-06-04 22:15:27 +0900

    feat : EventPage - Mission

---
+++ src/Pages/EventPage/Components/MissionMenuPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "../../../util/Container";
+
+function MainMenuPage() {
+  return (
+    <div>
+      <Container>Mission</Container>
+    </div>
+  );
+}
+
+export default MainMenuPage;
--- src/Pages/EventPage/EventPage.js
+++ src/Pages/EventPage/EventPage.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
-import Header from '../../util/Layout/Components/Header';
-import { eventGetData } from '../../data/event';
-import MenuBar from './Components/MenuBar';
-import MainMenuPage from './Components/MainMenuPage';
-import MemberMenuPage from './Components/MemberMenuPage';
-import { memberListGetData } from '../../data/memberList';
+import React, { useState } from "react";
+import Header from "../../util/Layout/Components/Header";
+import { eventGetData } from "../../data/event";
+import MenuBar from "./Components/MenuBar";
+import MainMenuPage from "./Components/MainMenuPage";
+import MemberMenuPage from "./Components/MemberMenuPage";
+import MissionMenuPage from "./Components/MissionMenuPage";
+import { memberListGetData } from "../../data/memberList";
 function EventPage() {
+  const [eventInfo] = useState(eventGetData);
+  const [memberList] = useState(memberListGetData);
+  const [selectMenu, setSelectMenu] = useState(1);
 
-    const [eventInfo,] = useState(eventGetData);
-    const [memberList, ] = useState(memberListGetData);
-    const [selectMenu, setSelectMenu] = useState(1);
-
-    return (
-        <div>
-            <Header desc={`${eventInfo?.year}-${eventInfo.semester} ${eventInfo.professor} 교수님`} />
-            <MenuBar selectMenu={selectMenu} setSelectMenu={setSelectMenu} code={eventInfo.inviteCode} />
-            {
-                selectMenu === 1 ? <MainMenuPage /> :
-                    selectMenu === 2 ? <MemberMenuPage memberList={memberList.members} /> :
-                        // selectMenu === 3 ? <MemberMenuPage memberList={memberList.members} /> :
-                null
-            }
-        </div>
-    )
+  return (
+    <div>
+      <Header
+        desc={`${eventInfo?.year}-${eventInfo.semester} ${eventInfo.professor} 교수님`}
+      />
+      <MenuBar
+        selectMenu={selectMenu}
+        setSelectMenu={setSelectMenu}
+        code={eventInfo.inviteCode}
+      />
+      {selectMenu === 1 ? (
+        <MainMenuPage />
+      ) : selectMenu === 2 ? (
+        <MemberMenuPage memberList={memberList.members} />
+      ) : selectMenu === 3 ? (
+        <MissionMenuPage memberList={memberList.members} />
+      ) : null}
+    </div>
+  );
 }
-
 
 export default EventPage;
