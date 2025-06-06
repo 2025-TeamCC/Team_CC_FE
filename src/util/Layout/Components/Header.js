f4b092ef0abaa8530e24a7f7431eaa563cfaec0a
pangil5634 <oksk6681@naver.com>
2025-06-06 19:02:27 +0900

    fix : back button

--- src/util/Layout/Components/Header.js
+++ src/util/Layout/Components/Header.js
@@ -3,13 +3,23 @@ import PropTypes from 'prop-types';
 import styled from "styled-components"
 import { Container } from "../../Container"
 import LoginHeader from "./LoginHeader";
+import { useLocation, useNavigate } from 'react-router-dom';
 
-function Header({desc}) {
+function Header({ desc }) {
+    
+    const navigate = useNavigate();
+    const location = useLocation();
     return (
         <HeaderContainer>
             {
                 desc
-                    ? <Desc>{desc}</Desc>
+                    ? location?.pathname === "/eventlist" || location?.pathname === "/event/create" || location?.pathname === "/event/attend"
+                        ? <Desc> {desc}</Desc>
+                        :
+                            <div>
+                                <BackButton onClick={() => navigate("/eventlist")}>← 홈으로 가기</BackButton>
+                                <Desc> {desc}</Desc>
+                            </div>
                     : <LoginHeader/>
             }
         </HeaderContainer>
@@ -34,4 +44,18 @@ const HeaderContainer = styled(Container)`
 const Desc = styled.p`
     font-size: ${({ theme }) => theme.fontSizes.xl};
     font-weight: bold;
+`;
+
+const BackButton = styled.button`
+  background: none;
+  border: none;
+  font-size: 14px;
+  color: gray;
+  margin-top: 10px;
+  margin-left: 0;
+  display: block;
+  text-align: left;
+  cursor: pointer;
+
+  margin-bottom: 10px;
 `;
